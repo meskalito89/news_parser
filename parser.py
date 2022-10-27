f229dd96f63e9a6ff7f7bb4bb445b89917d198a1
meskalito89 <rokinea89mone@mail.ru>
2022-10-27 17:12:37 +0600

    main function

--- parser.py
+++ parser.py
@@ -3,6 +3,12 @@ from sqlalchemy.orm import mapper, sessionmaker
 from bs4 import BeautifulSoup as bs
 from models import Resource, Items
 import requests
+import pdb
+from urllib.request import urljoin
+from random import shuffle, random
+from time import sleep
+import dateparser
+from time import mktime, time
 
 engine = create_engine("sqlite:///db.sqlite", echo=False)
 meta = MetaData(engine)
@@ -16,9 +22,107 @@ mapper(Items, items)
 dbsession = sessionmaker(bind=engine)
 session = dbsession()
 
-class Parser(Resource):
-    def __init__(self):
-        super().__init__()
+class Parser:
+    def __init__(self,
+                RESOURCE_ID,
+                RESOURCE_URL,
+                top_tag,
+                bottom_tag,
+                title_cut,
+                date_cut,
+                current_page = 1,
+                *args,
+                **kwargs
+                ):
+        self.RESOURCE_ID = RESOURCE_ID
+        self.RESOURCE_URL = RESOURCE_URL
+        self.top_tag = top_tag
+        self.bottom_tag = bottom_tag
+        self.title_cut = title_cut
+        self.date_cut = date_cut
+        self.current_page = 1
+        self.is_ended = False
+        self.news_url = []
+        self.parsed_row = dict()
 
-parser = Parser()
+    def get_full_url(self, href: str) -> dict:
+        return urljoin(self.RESOURCE_URL+'/', str(href))
 
+    def get_news_links_from_page_n(self, n: int) -> list:
+        if self.is_ended:
+            raise requests.exceptions.ConnectionError()
+
+        self.current_page = n
+
+        page_of_all_news_link = self.get_full_url(n)
+        try:
+            response = requests.get(page_of_all_news_link)
+
+            if not page_of_all_news_link:
+                self.is_ended = True
+                return []
+            
+            bs_obj = bs(response.text, 'html.parser')
+            first_news_tag = bs_obj.select_one(self.top_tag)
+            news_tags = first_news_tag.find_next_siblings()
+            hrefs = [tag.find('a', href=True).get('href') for tag in news_tags]
+            full_links = [self.get_full_url(href) for href in hrefs]
+            self.news_url = full_links
+            shuffle(self.news_url)
+            return full_links
+
+        except requests.exceptions.ConnectionError() as e:
+            return []
+
+
+
+    def parse_row(self, url: str) -> dict:
+        row = dict()
+        try:
+            response = requests.get(url)
+            bsobj = bs(response.text, 'html.parser')
+            row['res_id'] = self.RESOURCE_ID
+            row['link'] = url
+            row['title'] = bsobj.select_one(self.title_cut).text.strip()
+            row['content'] = bsobj.select_one(self.bottom_tag).text.strip()
+
+            date_str = bsobj.select_one(self.date_cut).get('datetime')
+            date_obj = dateparser.parse(date_str)
+            row['nd_date'] = int(mktime(date_obj.timetuple()))
+
+            row['s_date'] = int(time())
+
+            row['not_date'] = date_obj.strftime("%Y-%m-%d")
+            self.parsed_row = row
+
+            return row
+
+        except requests.ConnectionError:
+            random_time = random() * 10 + 2
+            sleep(random_time)
+
+
+    def parse_all_rows_of_page_n(self, n: int) -> list:
+        news_links = self.get_news_links_from_page_n(n)
+        shuffle(news_links)
+        rows = []
+        for link in news_links:
+            try:
+                row = self.parse_row(link)
+                rows.append(row)
+
+            except requests.exceptions.ConnectionError as err:
+                continue
+        return rows
+
+
+    def save_row(self):
+        print(self.parsed_row)
+    
+def all_parser_is_ended(list_of_parsers: list)->bool:
+    if sum([parser.is_ended])
+
+if __name__ == "__main__":
+    resources = [res.__dict__ for res in session.query(Resource).all()]
+    parsers = [Parser(**re) for re in resources]
+    for 
